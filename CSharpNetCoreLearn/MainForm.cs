a2da7d845c122589df96c3d413e71959d18bc28f
tekintian <tekintian@gmail.com>
2021-03-26 18:47:35 +0800

    CSharp NetCore learning project

---
+++ CSharpNetCoreLearn/MainForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpNetCoreLearn
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        //页面初始化方法
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
