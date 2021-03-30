983416e51277187afd847fc439cbdc8e4847596e
tekintian <tekintian@gmail.com>
2021-03-30 20:31:43 +0800

    TreeView 递归省市数据示例
    Signed-off-by: tekintian <tekintian@gmail.com>

---
+++ LearnCSharp/TreeViewForm1.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace LearnCSharp
+{
+    partial class TreeViewForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(24, 27);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(270, 436);
+            this.treeView1.TabIndex = 0;
+            // 
+            // TreeViewForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 514);
+            this.Controls.Add(this.treeView1);
+            this.Name = "TreeViewForm1";
+            this.Text = "TreeViewForm1";
+            this.Load += new System.EventHandler(this.TreeViewForm1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
---
+++ LearnCSharp/TreeViewForm1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace LearnCSharp
+{
+    public partial class TreeViewForm1 : Form
+    {
+        public TreeViewForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void TreeViewForm1_Load(object sender, EventArgs e)
+        {
+            LoadData(treeView1.Nodes, 0);
+        }
+
+        private void LoadData(TreeNodeCollection nodes, int pid)
+        {
+            DataTable dt = GetDataTableByPid(pid);
+            if (dt.Rows.Count>0)
+            {
+                //遍历数据并加载到TreeView上面
+                foreach (DataRow row in dt.Rows)
+                {
+                    int id = Convert.ToInt32(row[0]);
+                    string name = row[1].ToString();
+
+                    TreeNode node = nodes.Add(name);
+                    node.Tag = id;
+                    //递归实现TreeView 省市节点加载
+                    LoadData(node.Nodes, id);
+                }
+            }
+        }
+
+        private DataTable GetDataTableByPid(int pid) {
+            string sql = "select id, name from tb_area where pid=@pid";
+
+            return Tools.SqlHelper.ExecuteDataTable(sql,CommandType.Text, new System.Data.SqlClient.SqlParameter("@pid", DbType.Int32) { Value = pid });
+        }
+    }
+}
