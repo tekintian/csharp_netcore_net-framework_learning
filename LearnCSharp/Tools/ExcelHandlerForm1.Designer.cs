6b5f76b1b661bf11203acad82566b82b162542fb
tekintian <tekintian@gmail.com>
2021-03-30 20:31:14 +0800

    Excel操作演示
    Signed-off-by: tekintian <tekintian@gmail.com>

---
+++ LearnCSharp/Tools/ExcelHandlerForm1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace LearnCSharp.Tools
+{
+    partial class ExcelHandlerForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Exce导出";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(94, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Exce导入";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(104, 247);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 38);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "数据库表导出到Excel";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(104, 354);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(142, 38);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Excel导出到数据库";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // ExcelHandlerForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 468);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "ExcelHandlerForm1";
+            this.Text = "Excel导入导出示例";
+            this.Load += new System.EventHandler(this.ExcelHandlerForm1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ LearnCSharp/Tools/ExcelHandlerForm1.cs
@@ -0,0 +1,147 @@
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LearnCSharp.Tools
+{
+    public partial class ExcelHandlerForm1 : Form
+    {
+        public ExcelHandlerForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void ExcelHandlerForm1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+        //保存excel
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //保存文件对话框
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.DefaultExt = ".xls";//默认后缀 xsl
+            //支持的文件后缀  xls或者xlsx
+            dlg.Filter = "Excel Worksheets|*.xls;*.xlsx";
+
+            DialogResult result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == DialogResult.OK)
+            {
+                // Open document
+                string filename = dlg.FileName;
+
+                //创建并初始化集合
+                List<Person> list = new List<Person>() {
+                    new Person(){Name="张三",Age=19,Email="zs@qq.com" },
+                    new Person(){ Name="李四",Age=20,Email="ls@qq.com"},
+                    new Person(){ Name="王五",Age=15,Email="ww@qq.com"}
+                };
+
+                WriteExcel(filename, list);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //打开文件对话框
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.DefaultExt = ".xls";//默认后缀 xsl
+            //打开对话框支持的文件后缀  xls或者xlsx
+            dlg.Filter = "Excel Worksheets|*.xls;*.xlsx";
+
+            DialogResult result =dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == DialogResult.OK)
+            {
+                // Open document
+                string filename = dlg.FileName;
+                ReadExcel(filename);
+            }
+        }
+        //将数据导出到excel文件
+        private void WriteExcel(string filename, List<Person> list) {
+            //把List集合的内容导出到excel
+
+            // 1.创建工作表对象
+            IWorkbook workbook = new HSSFWorkbook();
+
+            // 2.在工作表中创建工资表对象
+            ISheet sheet = workbook.CreateSheet("List fro person");
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                //创建一行
+                IRow row = sheet.CreateRow(i);
+                //将值放入行中
+                row.CreateCell(0).SetCellValue(list[i].Name);
+                row.CreateCell(1).SetCellValue(list[i].Age);
+                row.CreateCell(2).SetCellValue(list[i].Email);
+            }
+
+            // 3 把workbook对象写入到磁盘
+            using (FileStream fs = File.OpenWrite(filename))
+            {
+                workbook.Write(fs);
+            }
+            MessageBox.Show("写入成功!");
+
+        }
+
+        //读取excel
+        private void ReadExcel(string filename) {
+            //创建一个读取对象
+            using (FileStream fs = File.OpenRead(filename)) {
+                //把文件流中的数据读取到workbook对象中
+                IWorkbook workbook = new HSSFWorkbook(fs);
+
+                //遍历workbook中的每一个sheeet  NumberOfSheets 为表中sheet的个数
+                for (int i = 0; i < workbook.NumberOfSheets; i++)
+                {
+                    //获取每个工作表
+                   ISheet sheet =  workbook.GetSheetAt(i);
+
+                   Console.WriteLine("--------{0}-----------",sheet.SheetName);
+
+                    //遍历获取工作表中的每一行
+                    for (int r = 0; r <= sheet.LastRowNum; r++)
+                    {
+                        //获取到工作表中的每一行
+                        IRow  row = sheet.GetRow(r);
+
+                        /*
+                        //注意这里获取单元格的值 需要根据实际的数据类型来获取 如
+                        string c1 = row.GetCell(0).StringCellValue;//字符串单元格数据
+
+                        double c2 = row.GetCell(1).NumericCellValue;//浮点型单元格数据
+
+                        DateTime date = row.GetCell(2).DateCellValue;//日期型单元格数据
+                        */
+
+                        //遍历获取每行中的每个单元格 LastCellNum 获取的是单元格的数量+1
+                        for (int c = 0; c < row.LastCellNum; c++)
+                        {
+                            //获取每个单元格
+                            ICell cell = row.GetCell(c);
+                            //这里演示把每个单元格都当做字符串来处理
+                            string val = cell.ToString();
+                            Console.Write("{0}  |  ", val);
+                        }
+
+                        Console.WriteLine();//输出一个换行
+
+                    }
+                }
+
+            }
+        }
+    }
+}
