76bdf4442b8fc52da132396e142f1320f29c7e60
tekintian <tekintian@gmail.com>
2021-04-07 22:02:52 +0800

    自定义扩展,实现where功能
    Signed-off-by: tekintian <tekintian@gmail.com>

---
+++ ConsoleNFApp1/MyListExt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleNFApp1
+{
+    public static class MyListExt
+    {
+        //自定义扩展,实现where功能
+        public static List<string> MyWhere(this List<string> list, Func<string,bool> funcWhere)
+        {
+            List<string>  retlist = new List<string>();
+            foreach (var item in list)
+            {
+                //调用委托方法, 如果条件满足则将这个项目加到list中
+                if (funcWhere(item))
+                {
+                    retlist.Add(item);
+                }
+            }
+            return retlist;
+        }
+    }
+}
---
+++ ConsoleNFApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleNFApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> strList = new List<string>() { "2","5","88","7"};
+
+            //匿名函数
+            // var tmp =strList.Where(delegate(string aa) { return aa.CompareTo("7") < 0; });
+            //var tmp = strList.MyWhere(delegate (string aa) { return aa.CompareTo("7") <= 0; });
+
+            //lambda表达式
+            //var tmp = strList.MyWhere( (string aa)=> { return aa.CompareTo("7") <= 0; });
+            //单个参数和一行代码 可用省略花括号
+            var tmp = strList.MyWhere( aa => aa.CompareTo("7")<=0 );
+
+            foreach (var item in tmp)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
