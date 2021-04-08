b5378566d9ff1cd0e504971ac3f30c052372b1b7
tekintian <tekintian@gmail.com>
2021-04-08 14:26:21 +0800

    socket服务端通信
    Signed-off-by: tekintian <tekintian@gmail.com>

---
+++ SocketDemo1/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace SocketDemo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_ip = new System.Windows.Forms.TextBox();
+            this.textBox_port = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.textBox_log = new System.Windows.Forms.TextBox();
+            this.textBox_msg = new System.Windows.Forms.TextBox();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP:";
+            // 
+            // textBox_ip
+            // 
+            this.textBox_ip.Location = new System.Drawing.Point(113, 28);
+            this.textBox_ip.Name = "textBox_ip";
+            this.textBox_ip.Size = new System.Drawing.Size(151, 21);
+            this.textBox_ip.TabIndex = 1;
+            this.textBox_ip.Text = "192.168.2.9";
+            // 
+            // textBox_port
+            // 
+            this.textBox_port.Location = new System.Drawing.Point(323, 29);
+            this.textBox_port.Name = "textBox_port";
+            this.textBox_port.Size = new System.Drawing.Size(70, 21);
+            this.textBox_port.TabIndex = 3;
+            this.textBox_port.Text = "8888";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(282, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port:";
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(440, 27);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 4;
+            this.btn_start.Text = "开启服务";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // textBox_log
+            // 
+            this.textBox_log.Location = new System.Drawing.Point(64, 74);
+            this.textBox_log.Multiline = true;
+            this.textBox_log.Name = "textBox_log";
+            this.textBox_log.Size = new System.Drawing.Size(371, 196);
+            this.textBox_log.TabIndex = 5;
+            // 
+            // textBox_msg
+            // 
+            this.textBox_msg.Location = new System.Drawing.Point(64, 324);
+            this.textBox_msg.Multiline = true;
+            this.textBox_msg.Name = "textBox_msg";
+            this.textBox_msg.Size = new System.Drawing.Size(356, 73);
+            this.textBox_msg.TabIndex = 6;
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(440, 324);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(100, 29);
+            this.btn_send.TabIndex = 7;
+            this.btn_send.Text = "发送消息";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 476);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.textBox_msg);
+            this.Controls.Add(this.textBox_log);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.textBox_port);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_ip);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_ip;
+        private System.Windows.Forms.TextBox textBox_port;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.TextBox textBox_log;
+        private System.Windows.Forms.TextBox textBox_msg;
+        private System.Windows.Forms.Button btn_send;
+    }
+}
+
---
+++ SocketDemo1/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SocketDemo1
+{
+    public partial class Form1 : Form
+    {
+        private List<Socket> ClientProxySocketList = new List<Socket>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            StartSocketServer();
+        }
+
+        private void StartSocketServer()
+        {
+            //socket服务创建的逻辑
+            //1. 创建socket对象
+            Socket serverSocket = new Socket(
+                AddressFamily.InterNetwork, //设置网络寻址协议
+                SocketType.Stream,// 设置数据传输方式
+                ProtocolType.Tcp // 通信协议
+                );
+
+            textBox_log.Text = "\r\n创建服务端Socket对象\r\n" + textBox_log.Text;
+            
+            //2.绑定IP和端口
+            IPAddress ip = IPAddress.Parse(textBox_ip.Text.Trim());
+            IPEndPoint iPEndPoint = new IPEndPoint(ip,int.Parse(textBox_port.Text));
+            serverSocket.Bind(iPEndPoint);
+
+            //3.开启监听 
+            serverSocket.Listen(10);
+
+            //使用线程池执行监听客户端连接
+            ThreadPool.QueueUserWorkItem(MyAcceptClient, serverSocket);
+
+            //ThreadPool.QueueUserWorkItem(new WaitCallback(this.MyAcceptClient), serverSocket);
+
+
+            /*textBox_msg.Text = "\r\n开始接受客户端连接:" + textBox_msg.Text;
+            //4.开始接受客户端连接  会阻塞当前线程,一直等待客户端连接
+            Socket proxySocket = serverSocket.Accept();
+            textBox_msg.Text = $"\r\n有客户端{proxySocket.RemoteEndPoint.ToString()}已连接:" + textBox_msg.Text;
+
+            string str = DateTime.Now.ToString();
+            byte[] data = Encoding.UTF8.GetBytes(str);
+
+            //给客户端发生消息
+            proxySocket.Send(data,0,data.Length,SocketFlags.None);
+
+            //给对方发送一个关闭的信号
+            proxySocket.Shutdown(SocketShutdown.Both);
+            textBox_msg.Text = "\r\n关闭连接:" + textBox_msg.Text;
+            proxySocket.Close();
+
+            //关闭socket
+            serverSocket.Close();*/
+        }
+
+        private void MyAcceptClient(object state)
+        {
+            var serverSocket = (Socket)state;
+            while (true)
+            {
+                //接受客户端连接
+                Socket proxySocket = serverSocket.Accept();
+
+                //夸线程访问调用处理  根据要访问的控件判断是否需要Invoke
+                if (textBox_log.InvokeRequired)
+                {
+                    textBox_log.Invoke(new Action(()=> {
+                        textBox_log.Text = $"\r\n客户端{proxySocket.RemoteEndPoint.ToString()}已连接:" + textBox_log.Text;
+                    }));
+                }
+                else
+                {
+                    textBox_log.Text = $"\r\n客户端{proxySocket.RemoteEndPoint.ToString()}已连接:" + textBox_log.Text;
+                }
+                
+                ClientProxySocketList.Add(proxySocket);//将连接添加到集合
+
+                //接收客户端消息
+                ThreadPool.QueueUserWorkItem(ReceiveData, proxySocket);
+
+
+            }
+        }
+        private void ReceiveData(object state) 
+        {
+            Socket proxySocket=(Socket)state;
+
+            byte[] data = new byte[1024 * 1024];
+            while (true)
+            {
+                int realLen = proxySocket.Receive(data, 0, data.Length, SocketFlags.None);
+                if (realLen==0)
+                {
+                    proxySocket.Shutdown(SocketShutdown.Both);
+                    proxySocket.Close();
+                    ClientProxySocketList.Remove(proxySocket);
+                    textBox_log.Text = $"客户端{proxySocket.RemoteEndPoint.ToString()} 已下线 \r\n" + textBox_log.Text;
+                    return;
+                }
+                string fromClientMsg = Encoding.UTF8.GetString(data, 0, realLen);
+
+                if (textBox_log.InvokeRequired)
+                {
+                    textBox_log.Invoke(new Action(()=> {
+                        textBox_log.Text = $"接收到客户端{proxySocket.RemoteEndPoint.ToString()}的消息{fromClientMsg}\r\n" + textBox_log.Text;
+                    }));
+                }
+                else
+                {
+                    textBox_log.Text = $"接收到客户端{proxySocket.RemoteEndPoint.ToString()}的消息{fromClientMsg}\r\n" + textBox_log.Text;
+                }
+                
+            
+            
+            }
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            BatchSend();
+        }
+
+        //群发消息
+        public void BatchSend()
+        {
+            //群发消息
+            foreach (var socket in ClientProxySocketList)
+            {
+                if (socket.Connected)
+                {
+                    string txt = textBox_msg.Text;
+                    byte[] data = Encoding.UTF8.GetBytes(txt);
+
+                    socket.Send(data, SocketFlags.None);
+                }
+                else
+                {
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                    ClientProxySocketList.Remove(socket);//将他从list中移除
+                }
+
+            }
+        }
+    }
+}
